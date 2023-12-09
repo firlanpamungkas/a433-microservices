7ef1da22474d1a2d2851ba4d41006b02595662a3
Firlana Tri Septia Pamungkas <firlan.atsp@gmail.com>
2023-12-09 15:35:32 +0700

    build push image karsajobs-ui

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,8 @@
+#building the image
+docker build . -t ghcr.io/firlanpamungkas/karsajobs-ui:latest
+
+#cli login
+echo $CR_PAT | docker login ghcr.io -u firlanpamungkas --password-stdin
+
+#push to the specified repo
+docker push ghcr.io/firlanpamungkas/karsajobs-ui:latest
